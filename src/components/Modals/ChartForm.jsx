23c168f49e5bc6c95491881cf206db227ed8f5ea
dunnoconnor <moconnor8@gmail.com>
2021-09-30 14:49:25 -0400

    intital data vis validation

--- src/components/Modals/ChartForm.jsx
+++ src/components/Modals/ChartForm.jsx
@@ -1,62 +1,65 @@
 import React from 'react';
 
-const ChartForm = ({type, options, handleChange}) => {
+const ChartForm = ({data, options, handleChange}) => {
+    console.log(options)
 
-    if (type==="LineChart"){
+    if (data.chart_type==="LineChart"){
         return(
 
             <div>
                 <p>A line chart requires two number fields to compare</p>
                 <p>The chart displays the two number fields compared along the x-axis and y-axis</p>
                 <p>x axis</p>
-                <select id='x_axis' onChange={handleChange}>
+                <select id='x_axis' value={data.x_axis} onChange ={handleChange}>
                     {options.field1.map(option => {
                         return <option key={option} value={option}>{option}</option>
                     })}
                 </select>
                 <p>y axis</p>
-                <select id='y_axis' onChange={handleChange}>
+                <select id='y_axis' value={data.y_axis} onChange={handleChange}>
                     {options.field2.map(option => {
                         return <option key={option} value={option}>{option}</option>
                     })}
                 </select>
             </div>
         )
-    } else if (type==="BarChart"){
+    } else if (data.chart_type==="BarChart"){
         return(
             <div>
                 <p>A bar chart requires one text field and one number field</p>
                 <p>The chart displays the value of the number field categorized by the selected text field</p>
-            <select id='y_axis' onChange={handleChange}>
+            <p>Bar Category</p>
+            <select id='y_axis' value={data.y_axis} onChange={handleChange}>
                 {options.field1.map(option => {
                     return <option key={option} value={option}>{option}</option>
                 })}
             </select>
-            <select id='x_axis' onChange={handleChange}>
+            <p>Bar Length</p>
+            <select id='x_axis' value={data.x_axis} onChange={handleChange}>
                 {options.field2.map(option => {
                     return <option key={option} value={option}>{option}</option>
                 })}
             </select>
         </div>
         )
-    } else if (type==="Histogram"){
+    } else if (data.chart_type==="Histogram"){
         return(
             <div>
                 <p>A histogram requires one number field</p>
                 <p>The chart displays the frequency of each value of that number field</p>
-                <select id='x_axis' onChange={handleChange}>
+                <select id='x_axis' value={data.x_axis} onChange={handleChange}>
                     {options.field1.map(option => {
                         return <option key={option} value={option}>{option}</option>
                     })}
                 </select>
             </div>
         )
-    } else if (type==="PieChart"){
+    } else if (data.chart_type==="PieChart"){
         return(
         <div>
             <p>A pie chart requires one text field</p>
             <p>The chart displays the frequency of each value of that text field</p>
-            <select id='x_axis' onChange={handleChange}>
+            <select id='x_axis' value={data.x_axis} onChange={handleChange}>
                 {options.field1.map(option => {
                     return <option key={option} value={option}>{option}</option>
                 })}
