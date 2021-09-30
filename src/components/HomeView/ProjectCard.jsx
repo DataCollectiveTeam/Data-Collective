2a3b9a61dd3ffacf28516ce05e6e691d2978928d
crener <cole.rener@outlook.com>
2021-09-30 11:20:51 -0400

    project view styled

--- src/components/HomeView/ProjectCard.jsx
+++ src/components/HomeView/ProjectCard.jsx
@@ -6,16 +6,14 @@ function ProjectCard({p}) {
     //card component for each project in homeview and user view
     return (
         <Link className="ProjectCard" to={`/projects/${p.id}`} key={p.id}>
-            <div className="card">
-                <div className="card-title">
-                    <h3>{p.name}</h3>
-                </div>
-                <div className="card-image">
-                    <img src={p.img} alt={p.name} />
-                </div>
-                <div className="card-body">
-                    <p>Contributors: {p.contributor_list.length}</p>
-                </div>
+            <div className="card-title">
+                <h3>{p.name}</h3>
+            </div>
+            <div className="card-image">
+                <img src={p.img} alt={p.name} />
+            </div>
+            <div className="card-body">
+                <p>Contributors: {p.contributor_list.length}</p>
             </div>
         </Link>
     );
